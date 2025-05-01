8510242e6af412e5e0c5242d36646fcd690e5fa4
Gustavo Gantois <gugagantois@gmail.com>
2025-04-30 21:11:32 -0300

    feat:Create routes login and register, and install dependecies.

---
+++ frontend-app/src/pages/login.jsx
@@ -0,0 +1,13 @@
+export default function Login() {
+    return (
+        <section className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            <figure className="bg-gradient-to-t from-black to-[#1b1b1b] h-screen borde-r-[40px]">
+            </figure>
+            <main className="flex justify-center items-center">
+                <div>
+                    
+                </div>
+            </main>
+        </section>
+    )
+}
