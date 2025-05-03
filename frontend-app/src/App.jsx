f7f3a100a1d406a40f9ed4dbe57616f016191515
Gustavo Gantois <gugagantois@gmail.com>
2025-05-03 16:38:51 -0300

    refactor:Refactor code main layout.

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from "./pages/login"
 import Register from "./pages/register"
 import Home from "./pages/platform/home"
+import { MainLayout } from "./pages/platform/main"
 
 function App() {
 
@@ -11,7 +12,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          } />
         </Routes>
       </BrowserRouter>
     </>
--- frontend-app/src/pages/platform/home.jsx
+++ frontend-app/src/pages/platform/home.jsx
@@ -1,13 +1,14 @@
-import { Sidebar } from "../../components/sidebar";
 import Banner from "/Banner.svg";
 
 export default function Home() {
     return (
         <section className="relative">
-           <Sidebar />
             <div className="max-w-[1440px] mx-auto p-8 lg:p-0">
-                <div className="absolute top-12 right-0 left-0 bottom-0">
-                    <img src={Banner} className="w-full h-60" alt="" />
+                <div>
+                
+                </div>
+                <div className="absolute top-20 right-80 left-0 bottom-0">
+                    <img src={Banner} className="w-full h-64" alt="" />
                 </div>
             </div>
         </section>
