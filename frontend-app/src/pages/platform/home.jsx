7b3af010a50d4088241466c74039f95339cf4f4e
Gustavo Gantois <gugagantois@gmail.com>
2025-05-03 16:31:47 -0300

    feat:Adding Banner.

--- frontend-app/src/pages/platform/home.jsx
+++ frontend-app/src/pages/platform/home.jsx
@@ -1,11 +1,14 @@
 import { Sidebar } from "../../components/sidebar";
+import Banner from "/Banner.svg";
 
 export default function Home() {
     return (
-        <section className="bg-[#F5F5F5]">
+        <section className="relative">
            <Sidebar />
             <div className="max-w-[1440px] mx-auto p-8 lg:p-0">
-
+                <div className="absolute top-12 right-0 left-0 bottom-0">
+                    <img src={Banner} className="w-full h-60" alt="" />
+                </div>
             </div>
         </section>
     )
