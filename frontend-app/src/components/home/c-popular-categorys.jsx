4372920105230eea4feec0f219fb72fec0e06593
Gustavo Gantois <gugagantois@gmail.com>
2025-05-05 18:43:15 -0300

    feat:Create header component.

---
+++ frontend-app/src/components/home/c-popular-categorys.jsx
@@ -0,0 +1,9 @@
+export function PopularCategorys({title, img}) { 
+    return (
+        <section>
+            <div className="flex flex-col items-center gap-2 lg:flex-row">
+                
+            </div>
+        </section>
+    )
+}
--- frontend-app/src/pages/platform/home.jsx
+++ frontend-app/src/pages/platform/home.jsx
@@ -1,16 +1,23 @@
+import { Header } from "../../components/home/c-header";
 import Banner from "/Banner.svg";
 
 export default function Home() {
-    return (
-        <section className="relative">
-            <div className="max-w-[1440px] mx-auto p-8 lg:p-0">
-                <div>
-                
-                </div>
-                <div className="absolute top-20 right-80 left-0 bottom-0">
-                    <img src={Banner} className="w-full h-64" alt="" />
-                </div>
-            </div>
+
+  return (
+    <section className="relative">
+      <div className="max-w-[1300px] mx-auto px-4 py-6 lg:px-2">
+        <Header />
+        {/* Banner */}
+        <div className="w-full mt-6 rounded-lg overflow-hidden hidden md:flex">
+          <img src={Banner} className="w- h-64 object-cover" alt="Banner promocional" />
+        </div>
+        <section className="mt-8">
+          <h2 className="font-semibold text-2xl text-[#E67E22] mb-4">
+            Categorias Populares
+          </h2>
+          {/* Aqui você pode mapear categorias futuras */}
         </section>
-    )
+      </div>
+    </section>
+  );
 }
