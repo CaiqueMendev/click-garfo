e4ea7f8625d933d63bc5f64302723c7b107c9d1f
Gustavo Gantois <gugagantois@gmail.com>
2025-05-06 16:58:23 -0300

    feat: Create header in favorite page.

--- frontend-app/src/pages/platform/favorites.jsx
+++ frontend-app/src/pages/platform/favorites.jsx
@@ -1,6 +1,12 @@
+import { Header } from "../../components/favorites/c-header";
+
 export default function MyFavorites() {
     return (
-        <main>
+        <main className="max-w-[1400px] mx-auto p-8 lg:p-0 mt-8 lg:mt-0">
+            <div className="mt-8 mb-8">
+                <Header />
+            </div>
+            
         </main>
     )
 }
