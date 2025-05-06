9dddc52ef3c98177a56f9fac42ed5f0f6a8625c4
Gustavo Gantois <gugagantois@gmail.com>
2025-05-05 21:02:00 -0300

    feat:Create Sales off component.

--- frontend-app/src/data/v-data.js
+++ frontend-app/src/data/v-data.js
@@ -1,3 +1,29 @@
+import { Heart, ShoppingCart } from "lucide-react";
+
+
+export const dataSalesOff = [
+  {
+    img: "/FrenchFries.svg",
+    title: "Batata Frita Clássica com Ketchup",
+    description: "Aquele sabor que nunca sai de moda! Porção generosa de batatas fritas crocantes por fora e macias por dentro, servidas com nosso ketchup especial"
+  },
+  {
+    img: "/hamburguerSales.svg",
+    title: "Combo Crocância Total",
+    description: "Uma explosão de sabor em cada mordida! Deliciosas batatas fritas em corte rústico, temperadas com nosso mix secreto de especiarias e acompanhadas de um potinho extra de ketchup artesanal. Perfeito para dividir... ou não!"
+  },
+  {
+    img: "/hamburguerLove.svg",
+    title: "Hamburguer Lovers – Porção Dupla",
+    description: "Para os verdadeiros fãs de batata frita: uma porção dupla bem servida, acompanhada de dois tipos de ketchup — o tradicional e o picante. Crocância, sabor e aquele toque apimentado que faz toda a diferença!"
+  },
+  {
+    img: "/kidsLunch.svg",
+    title: "Prato Kids porção divertida",
+    description: "Para os verdadeiros fãs de batata frita: uma porção dupla bem servida, acompanhada de dois tipos de ketchup — o tradicional e o picante. Crocância, sabor e aquele toque apimentado que faz toda a diferença!"
+  }
+]
+
 export const dataCategory = [
     {
       img: "/hamburguer.svg",
@@ -35,4 +61,55 @@ export const dataCategory = [
       img: "/refrigerantes.svg",
       title: "Refrigerante",
     }
+  ];
+
+  export const dataOrderFood = [
+    {
+      like: Heart,
+      img: "/BigBossBurguer.svg",
+      title: "🍔 BigBoss Burger",
+      description: "Um hambúrguer suculento com duas carnes, queijo cheddar derretido, bacon crocante e nosso molho especial da casa.",
+      price: "R$ 29,90",
+      icon: ShoppingCart,
+    },
+    {
+      like: Heart,
+      img: "/ChickenCrunch.svg",
+      title: "🍗 Chicken Crunch",
+      description: "Tiras de frango empanadas super crocantes, servidas com molho barbecue artesanal. Ideal para compartilhar!",
+      price: "R$ 34,90",
+      icon: ShoppingCart,
+    },
+    {
+      like: Heart,
+      img: "/SmokyDog.svg",
+      title: "🌭 Smoky Dog",
+      description: "Cachorro-quente no pão artesanal, com salsicha defumada, cebola crispy, cheddar cremoso e molho de mostarda dijon.",
+      price: "R$ 19,90",
+      icon: ShoppingCart,
+    },
+    {
+      like: Heart,
+      img: "/FritasSupremy.svg",
+      title: "🍟 Fritas Supreme",
+      description: "Batatas fritas temperadas com páprica, cobertas com cheddar derretido, bacon e cebolinha.",
+      price: "R$ 20,90",
+      icon: ShoppingCart,
+    },
+    {
+      like: Heart,
+      img: "/RefriGeladao.svg",
+      title: "🥤 Refri Geladão",
+      description: "Escolha entre Coca-Cola, Guaraná ou Fanta bem gelados para refrescar o seu pedido!",
+      price: "R$ 9,90",
+      icon: ShoppingCart,
+    },
+    {
+      like: Heart,
+      img: "/RefriGeladao.svg",
+      title: "🍔 Big Combo",
+      description: "Big combo acompanhado com hamburguer batata-frita molho especial e uma coca-cola de 2 litros.",
+      price: "R$ 50,00",
+      icon: ShoppingCart,
+    },
   ]
